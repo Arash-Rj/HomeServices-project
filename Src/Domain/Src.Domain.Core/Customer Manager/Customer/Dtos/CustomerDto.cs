a7806e67c048757007c846346c819b58824547ca
Arash <rajabiarash36@gmail.com>
2025-02-14 15:40:01 +0330

    Added Customer And Request Dto

---
+++ Src/Domain/Src.Domain.Core/Customer Manager/Customer/Dtos/CustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using Src.Domain.Core.Customer_Manager.Customer.Entities;
+using Src.Domain.Core.Expert_Manager.Expert.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Src.Domain.Core.Customer_Manager.Customer.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<AppRequest>? Requests { get; set; }
+        public List<FeedBack>? FeedBacks { get; set; }
+    }
+}
