d723e4a2a83104ab101739d3a7bf95470be130f6
Arash <rajabiarash36@gmail.com>
2025-02-16 00:30:42 +0330

    Added AdminTemplate Files

--- Src/EndPoints/Src.EndPoints.MVC.HomeService/Areas/Admin/Controllers/AdminController.cs
+++ Src/EndPoints/Src.EndPoints.MVC.HomeService/Areas/Admin/Controllers/AdminController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Src.Domain.Core.AAM.ManageUser.AppService;
+using Src.EndPoints.MVC.HomeService.Areas.Admin.Models;
+using Src.EndPoints.MVC.HomeService.Models;
 
 namespace Src.EndPoints.MVC.HomeService.Areas.Admin.Controllers
 {
+    [Authorize(Roles ="Admin")]
+    [Area("Admin")]
     public class AdminController : Controller
     {
+        private readonly IUserAppService _userAppService;
+        public AdminController(IUserAppService userAppService)
+        {
+            _userAppService = userAppService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Login()
+        {
+            var loginmodel = new AdminLoginModel();
+            return View(loginmodel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(AdminLoginModel adminLoginModel, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(adminLoginModel);
+            }
+            var result = await _userAppService.Login(adminLoginModel.UserName, adminLoginModel.Password, adminLoginModel.RememberMe, cancellationToken);
+            if (!result.Success)
+            {
+                ViewBag.ErrorMessage = result.Message;
+                return View();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Src/EndPoints/Src.EndPoints.MVC.HomeService/Areas/Admin/Models/AdminLoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Src.EndPoints.MVC.HomeService.Areas.Admin.Models
+{
+    public class AdminLoginModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "وارد کردن نام کاربری اجباری میباشد.")]
+        public string UserName { get; set; }
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "وارد کردن رمز عبور اجباری میباشد.")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
